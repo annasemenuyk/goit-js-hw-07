0457859a139196435b18f7461a410dbca92583af
annasemenuyk <83462012+annasemenuyk@users.noreply.github.com>
2021-10-01 09:50:10 +0300

    дз7

--- src/js/dist/task-01.dev.js
+++ src/js/dist/task-01.dev.js
@@ -1,36 +1,10 @@
+"use strict";
+
 //Посчитает и выведет в консоль количество категорий в ul#categories,
 // то есть элементов li.item. Получится 'В списке 3 категории.'.
-const totalLiElem = document.querySelectorAll('.item');//всі категорії
-console.log(`В списке ${totalLiElem.length} категории.`);
-//Для каждого элемента li.item в списке ul#categories, найдет и 
-//выведет в консоль текст заголовка элемента (тега h2) и 
-//количество элементов в категории (всех вложенных в него элементов li).
-const categoriesFirstLi = Array.from(
-    document.querySelectorAll(".categories > li")
-  );
-  const categoriesUl = Array.from(
-    document.querySelectorAll(".categories  li ul")
-  );
-  
-  const categoriesList = [];
-  
-  categoriesFirstLi.forEach(element => {
-    categoriesList.push(
-      `Категория: ${element.firstChild.textContent.trim()} / Количество: ${
-        element.firstElementChild.children.length
-      }`
-    );
-  });
-  
-  console.log(categoriesList);
- /*  const categoriesArray = [...totalCategories]
-  .map(
-    categories => `Категория: ${categories.children[0].textContent}
-Количество элементов: ${categories.children[1].children.length}`
-  )
-  .join("\n");
-console.log(categoriesArray); */
-  
+var totalLiElem = document.querySelectorAll('.item'); //всі категорії
+
+console.log("\u0412 \u0441\u043F\u0438\u0441\u043A\u0435 ".concat(totalLiElem, " \u043A\u0430\u0442\u0435\u0433\u043E\u0440\u0438\u0438."));
 /* Напиши скрипт, который выполнит следующие операции.
 
 Посчитает и выведет в консоль количество категорий в ul#categories, то есть элементов li.item. Получится 'В списке 3 категории.'.
