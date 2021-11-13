93ded80c6a9e3adcdab322cb5ba89d5e0197f2b1
annasemenuyk <83462012+annasemenuyk@users.noreply.github.com>
2021-11-13 23:17:11 +0200

    1

---
+++ js/02-lightbox.js
@@ -0,0 +1,6 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+const basicLightbox = require('basiclightbox')
+import * as basicLightbox from 'basiclightbox'
