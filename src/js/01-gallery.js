0dadb4441884baaa1b3d077625655b351da3220d
annasemenuyk <83462012+annasemenuyk@users.noreply.github.com>
2021-11-13 21:31:40 +0200

    разметка

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,3 +2,48 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+import refs from './refs.js'
+//console.log(refs);
+const {list, modal, button, modalClose, modalImage, modalOverlay} =refs;
+//console.log(list, modal, button);
+// Создание и рендер разметки по массиву данных и предоставленному шаблону.
+const item =
+    ` <li class="gallery__item">
+        <a
+          class="gallery__link"
+          href="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg"
+        >
+          <img
+            class="gallery__image"
+            src="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546__340.jpg"
+            data-source="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg"
+            alt="Tulips"
+          />
+        </a>
+      </li> `
+function createItems (arrey) {
+    return arrey
+    .map((el)=>{
+        const {preview, original, description} = el
+        //console.log (preview, original, description)
+            return `
+        <li class="gallery__item">
+          <a
+        class="gallery__link"
+        href=${original}
+      >
+          <img
+                class="gallery__image"
+                src=${preview}
+                data-source=${original}
+                alt=${description}
+          />
+          </a>
+        </li> `
+    })
+    .join('')
+   }
+const markup = createItems (galleryItems)
+//console.log (markup)
+list.insertAdjacentHTML("afterbegin", markup)
+const items = [...list.children]
---
+++ src/js/refs.js
@@ -0,0 +1,10 @@
+export default {
+    list: document.querySelector('.js-gallery'), //загальна галерея
+    modal: document.querySelector('.js-lightbox'), //модальне вікно
+    button: document.querySelector('.lightbox__button'), //кнопка закриття модального вікна
+    modalClose: document.querySelector('[data-action="close-lightbox"]'),//кнопка закриття модального вікна ч/з дата атрибут
+    modalImage: document.querySelector('.lightbox__image'), //картинка без атрибуту src
+    modalOverlay: document.querySelector('.lightbox__overlay'), //фон в модалці
+    sliderArrowRight: document.querySelector('[data-action="forward"]'),// кнопка слайдера вперед
+    sliderArrowLeft: document.querySelector('[data-action="backward"]')//кнопка слайдера назад
+    }
