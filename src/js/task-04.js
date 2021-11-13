203352371a0084d062c955e66830d9807df49845
annasemenuyk <83462012+annasemenuyk@users.noreply.github.com>
2021-11-13 21:20:09 +0200

    1

--- src/js/task-04.js
+++
@@ -1,8 +0,0 @@
-const btnDecrement = document.querySelector('[data-action="decrement"]');
-const btnIncrement = document.querySelector('[data-action="increment"]');
-const spanVal = document.querySelector("#value");
-let counterValue = 0;
-const increment = () => (spanVal.textContent = counterValue += 1);
-const decrement = () => (spanVal.textContent = counterValue -= 1);
-btnDecrement.addEventListener("click", decrement);
-btnIncrement.addEventListener("click", increment);
