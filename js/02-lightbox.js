7b00d08aeab009f379d0922bed59a9a9a3cc85d2
annasemenuyk <83462012+annasemenuyk@users.noreply.github.com>
2021-11-14 16:45:55 +0200

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,8 +2,8 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
-const basicLightbox = require('basiclightbox')
-import * as basicLightbox from 'basiclightbox'
+// const basicLightbox = require('basiclightbox')
+// import * as basicLightbox from 'basiclightbox'
 const item =
 ` <div class="gallery__item">
   <a class="gallery__link" href="large-image.jpg">
