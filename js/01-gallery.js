a8b8b9ca1779eff4483146f0fa9ea77a9af62f71
annasemenuyk <83462012+annasemenuyk@users.noreply.github.com>
2021-11-14 15:46:50 +0200

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,9 +1,9 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-// console.log(galleryItems);
+console.log(galleryItems);
 // const basicLightbox = require('basiclightbox');
-// import * as basicLightbox from 'basiclightbox';
+// import * as basicLightbox from 'basiclightbox';// 
 //Создание и рендер разметки по массиву данных galleryItems и 
 //предоставленному шаблону элемента галереи.
 const itemCards =
