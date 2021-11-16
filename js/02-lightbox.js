b1dfa1b9122c1f9542b2405e35d6dada43b3456c
annasemenuyk <83462012+annasemenuyk@users.noreply.github.com>
2021-11-16 11:29:02 +0200

    2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -4,40 +4,33 @@ import { galleryItems } from './gallery-items.js';
 console.log(galleryItems);
 // const basicLightbox = require('basiclightbox')
 // import * as basicLightbox from 'basiclightbox'
-const item =
-` <div class="gallery__item">
-  <a class="gallery__link" href="large-image.jpg">
-    <img
-      class="gallery__image"
-      src="small-image.jpg"
-      data-source="large-image.jpg"
-      alt="Image description"
-    />
-  </a>
-</div> `
-function createItemCards (arrey){
-    return arrey.map((el)=>{
-        const {description, original, preview} = el
-        //console.log (el)
-            return`
-            <div class="gallery__item">
-            <a class="gallery__link" href=${original}>
-              <img
-                class="gallery__image"
-                src=${preview}
-                data-source=${original}
-                alt=${description}
-              />
-            </a>
-          </div>`
-  })
-   .join('')
-}
-const markup = createItemCards (galleryItems);
-// console.log (markup);
-const list = document.querySelector('.gallery');
-list.insertAdjacentHTML("afterbegin", markup);
-const items = [...list.children];
+// const item =
+// `<a class="gallery__item" href="large-image.jpg">
+// <img class="gallery__image" src="small-image.jpg" alt="Image description" />
+// </a> `
+// function createItemCards (arrey){
+//     return arrey.map((el)=>{
+//         const {description, original, preview} = el
+//         //console.log (el)
+//             return`
+//             <div class="gallery__item">
+//             <a class="gallery__link" href=${original}>
+//               <img
+//                 class="gallery__image"
+//                 src=${preview}
+//                 data-source=${original}
+//                 alt=${description}
+//               />
+//             </a>
+//           </div>`
+//   })
+//    .join('')
+// }
+// const markup = createItemCards (galleryItems);
+// // console.log (markup);
+// const list = document.querySelector('.gallery');
+// list.insertAdjacentHTML("afterbegin", markup);
+// const items = [...list.children];
 list.addEventListener('click', onGoLink)
 function onGoLink (event) {
    event.preventDefault()
