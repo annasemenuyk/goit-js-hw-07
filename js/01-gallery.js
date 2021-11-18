ed32f99a1d88b8dd7e7957229e09cbfcb390daa8
annasemenuyk <83462012+annasemenuyk@users.noreply.github.com>
2021-11-18 15:57:16 +0200

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -42,22 +42,43 @@ const list = document.querySelector('.gallery');
 list.insertAdjacentHTML("afterbegin", markup);
 const items = [...list.children];
 console.log(items)
-
-list.addEventListener(`click`, noGoLink)
-function noGoLink (event) {
-   event.preventDefault()
-  };
  //Реализация делегирования на div.gallery и получение url большого изображения
 //  Открытие модального окна по клику на элементе галереи. Для этого ознакомься с документацией и примерами.
+
+list.addEventListener(`click`, onGalleryItemClick)
 const instance = basicLightbox.create(`
-    <img src="assets/images/image.png" width="800" height="600">
-`);
-list.addEventListener(`click`, onclick)
-function onclick (event) {
-  basicLightbox.create(event.target)
-  };
-list.onclick = () => {
-  basicLightbox.create(`
-  <img src=${items.original} width="800" height="600">
-`).show()
+   <img src="assets/images/image.png" width="800" height="600">
+ `);
+function onGalleryItemClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  galleryItems.map((item) => {
+    if (item.original === event.target.dataset.source) {
+      const instance = basicLightbox.create(` 
+        <img src=${item.original} width="800" height="600">
+    `);
+    instance.show();
+    }
+   }
+   )   
+ }
+// .addEventListener(`click`, onEscKeyPress)
+//   function onEscKeyPress(event) {
+//     const visible = instance.visible()
+//     const ESC_KEY_CODE = 'Escape';
+//     const pressEscKey = event.code === ESC_KEY_CODE;
+//     if (pressEscKey||) {
+//     instance.close()// close modal here;
+//     }
+//   }
+const imageEsc = document.querySelector('.gallery__image')
+imageEsc.addEventListener('keydown', onEscKeyPress)
+  function onEscKeyPress(event) {
+    const ESC_KEY_CODE = 'Escape';
+    const pressEscKey = event.code === ESC_KEY_CODE;
+    if (pressEscKey) {
+      instance.close()
+    }
   }
